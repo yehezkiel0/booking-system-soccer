12d73a4d6cd0f78657c29e97b353b4eabc7626e4
Yehezkiel Imannuel <91413725+yehezkiel0@users.noreply.github.com>
2026-01-03 11:34:17 +0700

    feat: create middleware setup (rate limiter, handle panic, validate api-key, validate token

--- user-service/constants/error/general.go
+++ user-service/constants/error/general.go
@@ -5,7 +5,7 @@ import "errors"
 var (
 	ErrInternalServerError = errors.New("internal server error")
 	ErrSQLError            = errors.New("database server failed to execute query")
-	ErrTooManyRequest      = errors.New("too many request")
+	ErrTooManyRequests     = errors.New("too many request")
 	ErrUnauthorized        = errors.New("unauthorized")
 	ErrInvalidToken        = errors.New("invalid token")
 	ErrForbidden           = errors.New("forbidden")
@@ -14,7 +14,7 @@ var (
 var GeneralError = []error{
 	ErrInternalServerError,
 	ErrSQLError,
-	ErrTooManyRequest,
+	ErrTooManyRequests,
 	ErrUnauthorized,
 	ErrInvalidToken,
 	ErrForbidden,
